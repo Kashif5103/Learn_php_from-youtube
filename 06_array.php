8eb29602b831d20d0b29d4807cc0f939c1dc2ee2
Malik Kashif <kaptaan813@gmail.com>
2024-10-20 23:08:42 +0500

    lacture 6 is complete

---
+++ 06_array.php
@@ -0,0 +1,13 @@
+<!-- array in the php -->
+<?php
+$roll_no=87;
+$roll_no=57;
+$roll_no=67;
+$roll_no=77;
+
+$roll_no=[45,90,87,65];
+
+echo $roll_no[3];
+foreach($roll_no as $roll_no){
+    echo $roll_no ."<br />";
+}
