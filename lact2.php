36a45c0d5fda6167deaa8c7301cebfa69344f6c5
Malik Kashif <kaptaan813@gmail.com>
2024-10-21 18:43:16 +0500

    add the lacture2 from the youtube

---
+++ lact2.php
@@ -0,0 +1,29 @@
+<?php
+
+// variable 
+$x=100;
+
+echo $x;
+
+$name="kashif";
+
+echo "</br>";
+
+echo $x;
+
+$x=10.5;
+
+var_dump($name);
+ 
+echo $x;
+
+var_dump($x);
+
+
+?>
+
+<!-- types of error -->
+
+<!-- 1. Fatal type -->
+ <!-- 2.Warning type -->
+  <!-- 3.Noticee types -->
