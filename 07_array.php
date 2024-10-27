88c1e1f302efd7d0698d5bdfc1dec2503b538262
Malik Kashif <kaptaan813@gmail.com>
2024-10-27 23:14:51 +0500

    complete the lacture 7

---
+++ 07_array.php
@@ -0,0 +1,15 @@
+<!-- array -->
+<!-- // array has a special variable which the value on the index  -->
+
+<!-- 90 % question is answer -->
+<!-- //30 langugages 
+// constant -->
+<!-- array in the php  -->
+ <!-- index array -- array with a numeric index -->
+  <!--associtive array--array with names keys  -->
+  <!-- Multinementional arrar --array contaning one or more arrays -->
+<?php
+$student=["irfan",'forqan','usman']; //index array
+var_dump($student);
+$student=["irfan",'name'=>'forqan','usman']; //Associative  array
+var_dump($student);
